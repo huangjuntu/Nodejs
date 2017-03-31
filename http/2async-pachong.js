19c0597afdaea7ddc42d213cfbd5c86278b44f60
huangjuntu <1721053474@qq.com>
2017-03-31 17:23:38 +0800

    爬虫注释加入

--- http/2async-pachong.js
+++ http/2async-pachong.js
@@ -1,19 +1,19 @@
-var http = require('http');
+var http = require('http');//把http模块require进来
 var url = 'http://www.imooc.com/learn/348';
-var cheerio = require('cheerio');
+var cheerio = require('cheerio');//把下面的模块require进来
 function filterChapters(html){
-	var $ = cheerio.load(html);
+	var $ = cheerio.load(html);//在控制台安装cheerio模块，npm install cheerio
 	var chapters = $('.chapter');
 	var courseDate = [];
-	chapters.each(function(item){
+	chapters.each(function(item){//遍历拿到单独的每一章
 		var chapter = $(this);
-		var chapterTitle = chapter.find('strong').text();
-		var videos = chapter.find('.video').find('li');
+		var chapterTitle = chapter.find('strong').text();//找到标签里面的内容
+		var videos = chapter.find('.video').find('li');//内容里面的li
 		var chapterDate = {
 			chapterTitle : chapterTitle,
 			videos : []
 		}
-		videos.each(function(item){
+		videos.each(function(item){//video遍历
 			var video = $(this).find('.J-media-item');
 			var videoTitle = video.text();
 			var videoId = video.attr('href').split('video/')[1];
@@ -28,9 +28,9 @@ function filterChapters(html){
 }
 function printCourseDate(courseDate){
 	courseDate.forEach(function(item){
-		console.log(item.chapterTitle + '\n');
+		console.log(item.chapterTitle);
 		item.videos.forEach(function(video){
-				console.log('[' + video.id + ']' + video.title );	
+				console.log('[' + video.id + ']' + video.title);	
 		})
 	})
 }
